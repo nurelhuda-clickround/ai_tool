142cdd4bc1085e8f1b22009e3d78523758807b92
Nurelhuda El-Younis <nurelhuda.younis@click-round.com>
2025-11-12 13:50:39 +0200

    update

--- utils.py
+++ utils.py
@@ -38,20 +38,20 @@ import torch
 load_dotenv()
 
 # Access MySQL credentials
-MYSQL_HOST = os.getenv("MYSQL_HOST")
-MYSQL_USER = os.getenv("MYSQL_USER")
-MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
-MYSQL_DATABASE = os.getenv("MYSQL_DATABASE")
-MYSQL_PORT = os.getenv("MYSQL_PORT", "3306")
+# MYSQL_HOST = os.getenv("MYSQL_HOST")
+# MYSQL_USER = os.getenv("MYSQL_USER")
+# MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+# MYSQL_DATABASE = os.getenv("MYSQL_DATABASE")
+# MYSQL_PORT = os.getenv("MYSQL_PORT", "3306")
 
 # Access API credentials
 # API_BASE_URL = os.getenv("API_BASE_URL", "http://192.168.10.82/hxa/ai_api/index.php")
 # API_KEY = os.getenv("API_KEY")
-db_user = st.secrets["MYSQL_USER"] 
-db_password = st.secrets["MYSQL_PASSWORD"] 
-db_host = st.secrets["MYSQL_HOST"] 
-db_port = st.secrets["MYSQL_PORT"] 
-db_name = st.secrets["MYSQL_DATABASE"]
+db_user = st.secrets["mysql"]["MYSQL_USER"] 
+db_password = st.secrets["mysql"]["MYSQL_PASSWORD"] 
+db_host = st.secrets["mysql"]["MYSQL_HOST"] 
+db_port = st.secrets["mysql"]["MYSQL_PORT"] 
+db_name = st.secrets["mysql"]["MYSQL_DATABASE"]
 # Access OpenAI API key
 # api_key = os.getenv("OPENAI_API_KEY")
 # os.environ["OPENAI_API_KEY"] = api_key
